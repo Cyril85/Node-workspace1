eecbfd8c90e989747f4a36d4e6c130c2d8f71440
Cyril85 <cyrilthankachan85@gmail.com>
2018-03-04 10:17:15 +0000

    set up start script and heroku port

--- app_express.js
+++ app_express.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs=require('fs');
+const fs = require('fs');
 
 var app = new express();
-hbs.registerPartials(__dirname+'/views/partials');
-app.set('view engine','hbs');
-hbs.registerHelper('currentYear',()=>{return new Date().getFullYear()});
-hbs.registerHelper('capitalise',(text)=>{return text.toUpperCase()});
+var port = process.env.PORT || 3000;
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('capitalise', (text) => {
+    return text.toUpperCase()
+});
 //app.use((req,res,next)=>{ res.send('Under maintenance')});
 
 app.get('/', (req, res) => {
-    res.render('index.hbs',{site_name:'express trial'});
+    res.render('index.hbs', {site_name: 'express trial'});
 });
 
-app.listen(3000,()=>{
-    console.log('Server started listening on 3000');
+app.listen(port, () => {
+    console.log('Server started listening on ', port);
 });
